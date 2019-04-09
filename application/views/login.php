c347f8ec841ecd8b7c90eee133f949c139f47c13
firsticz <thanawat6368@gmail.com>
2019-04-09 21:25:54 +0700

    add

---
+++ application/views/login.php
@@ -0,0 +1,97 @@
+<html><head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login - Free Bulma template</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.css" rel="stylesheet">
+    <!-- Bulma Version 0.7.4-->
+    <!-- <link rel="stylesheet" type="text/css" href="login.css"> -->
+    <style>
+    html,body {
+    font-family: 'Open Sans', serif;
+    font-size: 14px;
+    font-weight: 300;
+  }
+  .hero.is-success {
+    background: #F2F6FA;
+  }
+  .hero .nav, .hero.is-success .nav {
+    -webkit-box-shadow: none;
+    box-shadow: none;
+  }
+  .box {
+    margin-top: 5rem;
+  }
+  .avatar {
+    margin-top: -70px;
+    padding-bottom: 20px;
+  }
+  .avatar img {
+    padding: 5px;
+    background: #fff;
+    border-radius: 50%;
+    -webkit-box-shadow: 0 2px 3px rgba(10,10,10,.1), 0 0 0 1px rgba(10,10,10,.1);
+    box-shadow: 0 2px 3px rgba(10,10,10,.1), 0 0 0 1px rgba(10,10,10,.1);
+  }
+  input {
+    font-weight: 300;
+  }
+  p {
+    font-weight: 700;
+  }
+  p.subtitle {
+    padding-top: 1rem;
+  
+    </style>
+</head>
+
+<body>
+    <section class="hero is-success is-fullheight">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <div class="column is-4 is-offset-4">
+                    <h3 class="title has-text-grey">Login</h3>
+                    <p class="subtitle has-text-grey">Please login to proceed.</p>
+                    <div class="box">
+                        <figure class="avatar">
+                            <img src="https://placehold.it/128x128">
+                        </figure>
+                        <form>
+                            <div class="field">
+                                <div class="control">
+                                    <input class="input is-large" type="email" placeholder="Your Email">
+                                </div>
+                            </div>
+
+                            <div class="field">
+                                <div class="control">
+                                    <input class="input is-large" type="password" placeholder="Your Password">
+                                </div>
+                            </div>
+                            <div class="field">
+                                <label class="checkbox">
+                  <input type="checkbox">
+                  Remember me
+                </label>
+                            </div>
+                            <button class="button is-block is-info is-large is-fullwidth" onclick="myFunction()">Login</button>
+                        </form>
+                    </div>
+                    <p class="has-text-grey">
+                        <a href="../">Sign Up</a> &nbsp;·&nbsp;
+                        <a href="../">Forgot Password</a> &nbsp;·&nbsp;
+                        <a href="../">Need Help?</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script>
+        function myFunction() {
+          location.replace("menu.html")
+        }
+        </script>
+</body>
+
+</html>
---
+++ application/views/menu.php
@@ -0,0 +1,277 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="main.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/stylesheet-2018-a42a36b295.css">
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
+</head>
+<body>
+<header>
+  <div class="header-location " ng-controller="headerLocationController">
+    <div class="container">
+      <div class="row">
+        <div class="header-logo">
+          <a href="https://1112.com"><img class="logo" src="img/test.png"></a>
+        </div>
+        <div class="header-location-container">
+
+
+          <div class="header-menu">
+            <div class="menu-right">
+              <ul class="menu screen">
+                                  <li class="user-menu no-border hidden-xs hidden-sm hidden-md">
+                    <a class="login" href="http://localhost/human/welcome/login"><i class="icon icon-user icon-hw"></i> เข้าสู่ระบบ</a> / <a class="signup" href="http://localhost/human/welcome/login">ลงทะเบียน</a>
+                  </li>
+                                <li class="lang">
+                                      <a href="">EN</a>
+                                  </li>
+                <li>
+                  <div id="cart" class="cart-btn" ng-controller="cartController">
+                    <a href="javascript:void(0);"><i class="icon icon-check icon-hw"></i> <span class="item-count">0</span></a>
+                    <div class="cart-container">
+	<div class="cart-empty">
+		<div class="cart-icon">
+			<i class="fa fa-shopping-basket"></i>
+		</div>
+		<h3>คุณยังไม่มีสินค้าในตะกร้า!</h3>
+		<p>คลิกที่ปุ่ม +<i class="fa fa-shopping-basket"></i> เพื่อใส่สินค้าลงในตะกร้า</p>
+	</div>
+		<div class="cart-item-list hide" style="max-height: 276px;">
+		<!---->
+	</div>
+
+				<div class="cart-summary-list delivery-charge" ng-show="deliveryType=='delivery'">
+			<div class="cart-summary-label">ค่าจัดส่ง</div>
+			<div class="cart-summary-value">
+				50 ฿			</div>
+		</div>
+		<div class="cart-summary-list cart-sub-total">
+			<div class="cart-summary-label">ราคารวม <br><small>ไม่รวมส่วนลด</small></div>
+			<div class="cart-summary-value"><span data-subtotal-price="0" data-total-price="50" id="cart-total-price">50</span> ฿</div>
+		</div>
+		<div class="checkout">
+			<a class="btn btn-red" href="javascript:void(0)" ng-click="checkout()">ทำการสั่งซื้อ</a>
+			
+		</div>
+	</div>
+</div>
+                  </div>
+
+                </li>
+              </ul>
+            </div>
+          </div>  
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="header-menu">
+      <div class="container">
+        <div class="toggle-menu">
+          <span class="icon-toggle"></span>
+          <span class="icon-toggle short"></span>
+          <span class="icon-toggle"></span>
+        </div>
+        <div class="menu-overlay"></div>
+        <div class="menu-left">
+          <div class="close-menu">
+            <span class="icon-close"></span>
+            <span class="icon-close"></span>
+          </div>
+          
+          <ul class="menu">
+
+                                      <li class="promo-menu">
+                <a href=""></a>
+              </li>
+              <li>
+                <a href="">ประเภทอาหาร</a>
+              </li>
+                        <li>
+              <a href="">พิซซ่า พัฟ</a>
+            </li>
+            <li>
+              <a href="">อาหารจานเดียว</a>
+            </li>
+            <li>
+              <a href="">สลัด</a>
+            </li>
+            <li>
+              <a href="">อาหารรองท้อง</a>
+            </li>
+            <li>
+              <a href="">ของหวานและเครื่องดื่ม</a>
+            </li>
+            
+           
+
+                        
+          </ul>
+
+          
+        </div>
+        <div class="menu-right menu mobile">
+                
+                </div>
+      </div>
+    </div>
+  </div>
+</header>
+
+
+
+      <section class="body-container">
+				<!-- <div class="promote-tab text-center ng-hide" ng-show="promote">
+    <div class="container">
+        <div class="promote-text ng-hide" ng-show="promote.text"></div>
+        <button class="promote-btn ng-hide" ng-show="promote.btn_status" ng-click="promoteApply()" ng-disabled="$root.promoteRoot.promote_apply"></button>
+    </div>
+</div> -->
+				<section class="product">
+	<div class="container item-container" ng-controller="itemController" ng-init="init()">
+		<h1 class="heading">อาหาร</h1>
+
+		
+		<div class="category">
+			<!----><h3 class="category-list ng-hide active" ng-class="(category.category_id == selected_category? 'active':'')" ng-repeat="category in brand_categories" ng-show="category.category_id != 0" ng-click="changeCategory(category.category_id)">
+				ไม่มีหมวดหมู่
+			</h3><!---->
+		</div>
+
+		<!----><div ng-repeat="category in brand_categories">
+			<div class="flex-container" ng-show="selected_category == category.category_id">
+				<!----><div class="flex-item" ng-repeat="item in items | filter: { brand_category: { category_id: category.category_id } }">
+					<div class="product-item">
+	<div class="item-image">
+		<img class="product-image" ng-src="img/pat.png" src="img/pat.png">
+	</div>
+	<div class="item-content-container">
+		<div class="item-name">
+			<h1>ข้าวผัด</h1>
+		</div>
+		
+	</div>
+</div>
+
+<div class="item-footer">
+		<button class="btn add-to-cart non-pizza-item btn-red btn-block " ng-click="addItem($event.target, item)" ng-disabled="!checkItemAvaiable(item)">
+			<span class="left price">129 ฿</span>
+			<span class="right txt"><i class="fa fa-plus"></i> <i class="fa fa-shopping-basket"></i></span>
+			<span class="loading">
+				<img src="https://1112.minorcdn.com/1112/public/images/spinner.svg">
+				<span class="small">กำลังใส่สินค้าลงตะกร้า</span>
+			</span>
+		</button>
+	</div>
+				</div><!----><div class="flex-item" ng-repeat="item in items | filter: { brand_category: { category_id: category.category_id } }">
+					<div class="product-item">
+	<div class="item-image">
+		<img class="product-image" ng-src="img/kai.png" src="img/kai.png">
+	</div>
+	<div class="item-content-container">
+		<div class="item-name">
+			<h1>ไก่ทอด</h1>
+		</div>
+		
+	</div>
+</div>
+
+<div class="item-footer">
+			
+		<button class="btn add-to-cart non-pizza-item btn-red btn-block " ng-click="addItem($event.target, item)" ng-disabled="!checkItemAvaiable(item)">
+			<span class="left price">129 ฿</span>
+			<span class="right txt"><i class="fa fa-plus"></i> <i class="fa fa-shopping-basket"></i></span>
+			<span class="loading">
+				<img src="https://1112.minorcdn.com/1112/public/images/spinner.svg">
+				<span class="small">กำลังใส่สินค้าลงตะกร้า</span>
+			</span>
+		</button>
+	</div>
+				</div><!----><div class="flex-item" ng-repeat="item in items | filter: { brand_category: { category_id: category.category_id } }">
+					<div class="product-item">
+	<div class="item-image">
+		<img class="product-image" ng-src="img/tomyum.png" src="img/tomyum.png">
+	</div>
+	<div class="item-content-container">
+		<div class="item-name">
+			<h1>ต้มยำกุ้ง</h1>
+		</div>
+		
+	</div>
+</div>
+
+<div class="item-footer">
+			
+		<button class="btn add-to-cart non-pizza-item btn-red btn-block " ng-click="addItem($event.target, item)" ng-disabled="!checkItemAvaiable(item)">
+			<span class="left price">129 ฿</span>
+			<span class="right txt"><i class="fa fa-plus"></i> <i class="fa fa-shopping-basket"></i></span>
+			<span class="loading">
+				<img src="https://1112.minorcdn.com/1112/public/images/spinner.svg">
+				<span class="small">กำลังใส่สินค้าลงตะกร้า</span>
+			</span>
+		</button>
+	</div>
+				</div><!----><div class="flex-item" ng-repeat="item in items | filter: { brand_category: { category_id: category.category_id } }">
+					<div class="product-item">
+	<div class="item-image">
+		<img class="product-image" ng-src="img/yum.png" src="img/yum.png">
+	</div>
+	<div class="item-content-container">
+		<div class="item-name">
+			<h1>ยำวุ้นเส้น</h1>
+		</div>
+		
+	</div>
+</div>
+
+<div class="item-footer">
+			
+		<button class="btn add-to-cart non-pizza-item btn-red btn-block " ng-click="addItem($event.target, item)" ng-disabled="!checkItemAvaiable(item)">
+			<span class="left price">129 ฿</span>
+			<span class="right txt"><i class="fa fa-plus"></i> <i class="fa fa-shopping-basket"></i></span>
+			<span class="loading">
+				<img src="https://1112.minorcdn.com/1112/public/images/spinner.svg">
+				<span class="small">กำลังใส่สินค้าลงตะกร้า</span>
+			</span>
+		</button>
+	</div>
+				</div><!----><div class="flex-item" ng-repeat="item in items | filter: { brand_category: { category_id: category.category_id } }">
+					<div class="product-item">
+	<div class="item-image">
+		<img class="product-image" ng-src="img/patk.png" src="img/patk.png">
+	</div>
+	<div class="item-content-container">
+		<div class="item-name">
+			<h1>ผัดกะเพรา</h1>
+		</div>
+		
+	</div>
+</div>
+
+<div class="item-footer">
+			
+		<button class="btn add-to-cart non-pizza-item btn-red btn-block " ng-click="addItem($event.target, item)" ng-disabled="!checkItemAvaiable(item)">
+			<span class="left price">129 ฿</span>
+			<span class="right txt"><i class="fa fa-plus"></i> <i class="fa fa-shopping-basket"></i></span>
+			<span class="loading">
+				<img src="https://1112.minorcdn.com/1112/public/images/spinner.svg">
+				<span class="small">กำลังใส่สินค้าลงตะกร้า</span>
+			</span>
+		</button>
+	</div>
+				</div><!---->
+			</div>
+		</div><!---->
+
+	</div>
+</section>
+			</section>
+
+</body>
+</html>
